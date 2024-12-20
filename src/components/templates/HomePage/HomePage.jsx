00a2af83cf3d5e78be3eb7fee4d70d4269465f51
Hadi Karimi <karimi1382hadi@gmail.com>
2024-12-21 00:54:09 +0330

    add homepage benefits section

--- src/components/templates/HomePage/HomePage.jsx
+++ src/components/templates/HomePage/HomePage.jsx
@@ -8,6 +8,10 @@ import bgBannerDesktop from "@/public/images/banner/banner-desktop.png";
 import bgBannerMobile from "@/public/images/banner/banner-mobile.png";
 import HomePageSlider from "@/components/modules/HomePage/HomePageSlider";
 
+import supportImage from "@/public/images/support.png";
+import userSatisfactionImage from "@/public/images/user-satisfaction.png";
+import mostEconomicalImage from "@/public/images/most-economical-price.png";
+
 function HomePage() {
   return (
     <>
@@ -24,7 +28,7 @@ function HomePage() {
         <HomePageSearch />
         <HomePageTours />
         <div className="mt-[100px]">
-          <div>
+          <div className="flex items-center">
             <Image
               src={bgBannerMobile}
               width={327}
@@ -42,6 +46,61 @@ function HomePage() {
           </div>
         </div>
         <HomePageSlider />
+      </div>
+      <div className="mx-[.75rem] mt-[50px] border-t border-black/25 lg:mx-0 lg:mt-[20px]">
+        <div className="container grid max-w-[400px] grid-cols-1 gap-[30px] py-[40px] lg:max-w-full lg:grid-cols-3 lg:px-[1.5rem] lg:py-[20px]">
+          <div className="flex items-center">
+            <Image
+              src={mostEconomicalImage}
+              width={122}
+              height={110}
+              className="h-[64px] w-[72px] lg:h-[100px] lg:w-[110px]"
+              alt="most economical price image"
+            />
+            <div>
+              <h3 className="text-[14px] font-medium lg:text-[26px]">
+                بصرفه ترین قیمت
+              </h3>
+              <p className="mt-[10px] text-[12px] lg:text-[16px]">
+                بصرفه ترین و ارزان ترین قیمت تور را از ما بخواهید.
+              </p>
+            </div>
+          </div>
+          <div className="flex items-center">
+            <Image
+              src={supportImage}
+              width={122}
+              height={110}
+              className="h-[64px] w-[72px] lg:h-[100px] lg:w-[110px]"
+              alt="support image"
+            />
+            <div>
+              <h3 className="text-[14px] font-medium lg:text-[26px]">
+                پشتیبانی
+              </h3>
+              <p className="mt-[10px] text-[12px] lg:text-[16px]">
+                پشتیبانی و همراهی 24 ساعته در تمامی مراحل سفر شما.
+              </p>
+            </div>
+          </div>
+          <div className="flex items-center">
+            <Image
+              src={userSatisfactionImage}
+              width={122}
+              height={110}
+              className="h-[64px] w-[72px] lg:h-[100px] lg:w-[110px]"
+              alt="user satisfaction image"
+            />
+            <div>
+              <h3 className="text-[14px] font-medium lg:text-[26px]">
+                رضایت کاربران
+              </h3>
+              <p className="mt-[10px] text-[12px] lg:text-[16px]">
+                رضایت بیش از 10هزار کاربر از تور های ما.
+              </p>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
