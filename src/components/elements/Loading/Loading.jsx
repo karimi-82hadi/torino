77a9c404b6d0361f819c970db61dfbcad86c7a39
Hadi Karimi <karimi1382hadi@gmail.com>
2024-12-20 21:02:32 +0330

    styles: set animation for Loading with css instead of animate tag
    fix: chnage some folder name.

--- src/components/elements/Loading/Loading.jsx
+++ src/components/elements/Loading/Loading.jsx
@@ -1,3 +1,5 @@
+import styles from "./Loading.module.css";
+
 function Loading({ fill, width, height }) {
   return (
     <svg
@@ -7,33 +9,27 @@ function Loading({ fill, width, height }) {
       viewBox="0 0 24 24"
       fill={fill || "#a1a3a8"}
     >
-      <circle cx="3" cy="3" r="3" transform="matrix(1 0 0 1 1 9)">
-        <animate
-          attributeName="opacity"
-          dur="1.2s"
-          values="1;.6;.3;1"
-          calcMode="linear"
-          repeatCount="indefinite"
-        ></animate>
-      </circle>
-      <circle cx="3" cy="3" r="3" transform="matrix(1 0 0 1 9 9)">
-        <animate
-          attributeName="opacity"
-          dur="1.2s"
-          values=".6;.3;1;.6"
-          calcMode="linear"
-          repeatCount="indefinite"
-        ></animate>
-      </circle>
-      <circle cx="3" cy="3" r="3" transform="matrix(1 0 0 1 17 9)">
-        <animate
-          attributeName="opacity"
-          dur="1.2s"
-          values=".3;1;.6;.3"
-          calcMode="linear"
-          repeatCount="indefinite"
-        ></animate>
-      </circle>
+      <circle
+        cx="3"
+        cy="3"
+        r="3"
+        transform="matrix(1 0 0 1 1 9)"
+        className={styles.Loading_Circle1}
+      />
+      <circle
+        cx="3"
+        cy="3"
+        r="3"
+        transform="matrix(1 0 0 1 9 9)"
+        className={styles.Loading_Circle2}
+      />
+      <circle
+        cx="3"
+        cy="3"
+        r="3"
+        transform="matrix(1 0 0 1 17 9)"
+        className={styles.Loading_Circle3}
+      />
     </svg>
   );
 }
